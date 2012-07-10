b23f6a59bcb2689bf148de7b5141c2088f5891b9
Erik Kallen <erikkallen@talisa-baass-macbook-3.local>
2012-07-10 20:34:57 +0200

    some portfolio leftovers

--- db/migrate/20120317143860_add_cover_image_id_to_portfolio_gallery.refinery_portfolio.rb
+++
@@ -1,8 +0,0 @@
-# This migration comes from refinery_portfolio (originally 4)
-class AddCoverImageIdToPortfolioGallery < ActiveRecord::Migration
-
-  def change
-    add_column :refinery_portfolio_galleries, :cover_image_id, :integer
-  end
-
-end
