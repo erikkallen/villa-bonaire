e1b63f4a96ba5cc500098915185340f8705f51db
Erik Kallen <erikkallen@talisa-baass-macbook-3.local>
2012-07-10 21:44:27 +0200

    changed locale setting

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -7,9 +7,9 @@ Refinery::I18n.configure do |config|
 
   config.current_locale = :nl
 
-   config.default_frontend_locale = :en
+   config.default_frontend_locale = :nl
 
-   config.frontend_locales = [:en]
+   config.frontend_locales = [:en,:nl]
 
   # config.locales = {:en=>"English", :fr=>"Français", :nl=>"Nederlands", :"pt-BR"=>"Português", :da=>"Dansk", :nb=>"Norsk Bokmål", :sl=>"Slovenian", :es=>"Español", :it=>"Italiano", :de=>"Deutsch", :lv=>"Latviski", :ru=>"Русский", :sv=>"Svenska", :pl=>"Polski", :"zh-CN"=>"Simplified Chinese", :"zh-TW"=>"Traditional Chinese", :el=>"Ελληνικά", :rs=>"Srpski", :cs=>"Česky", :sk=>"Slovenský", :ja=>"日本語", :bg=>"Български"}
 end
