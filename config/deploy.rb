a40c613b625d0cf7fa77c6933449ab922913997c
Erik Kallen <erikkallen@talisa-baass-macbook-3.local>
2012-07-14 17:38:18 +0200

    removed video

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,3 +49,12 @@ namespace :deploy do
   end
   before "deploy", "deploy:check_revision"
 end
+
+desc "tail production log files" 
+task :tail_logs, :roles => :app do
+  run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
+    puts  # for an extra line break before the host name
+    puts "#{channel[:host]}: #{data}" 
+    break if stream == :err    
+  end
+end
