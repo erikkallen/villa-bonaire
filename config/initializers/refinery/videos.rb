da98889b4e065a41b56cfb49e06bf89321bffb90
Erik Kallen <erikkallen@talisa-baass-macbook-3.local>
2012-07-14 11:55:15 +0200

    before layout chage

---
+++ config/initializers/refinery/videos.rb
@@ -0,0 +1,20 @@
+Refinery::Videos.configure do |config|
+    #Configures the maximum allowed upload size (in bytes) for an video file
+    #config.max_file_size = 524288000
+
+    # Configure how many videos per page should be displayed in the list of images in the admin area
+    #config.pages_per_admin_index =  20
+
+    # Configure white-listed mime types for validation
+    #config.whitelisted_mime_types = ["video/mp4", "video/x-flv", "application/ogg", "video/webm", "video/flv", "video/ogg"]
+
+    # Configure Dragonfly
+    # This is where in the middleware stack to insert the Dragonfly middleware
+    # config.dragonfly_insert_before = ActionDispatch::Callbacks
+    # config.dragonfly_secret = c8619d8f971a15e25eb8c0d2d9d5fda48e7e254a748c4413
+    # If you decide to trust file extensions replace :ext below with :format
+    # config.dragonfly_url_format = /system/videos/:job/:basename.:format
+    # config.datastore_root_path = /Users/erikkallen/Sites/villa-bonaire/public/system/refinery/videos
+    # config.trust_file_extensions = false
+
+end
