9470932e0b9546a784e4245259d3eb40ec3f562f
Erik Kallen <erikkallen@Eriks-oude-laptop.local>
2012-09-01 13:24:31 +0200

    added clocks captions background color

---
+++ vendor/extensions/reservations/app/mailers/refinery/reservations/reservation_mailer.rb
@@ -0,0 +1,23 @@
+module Refinery
+  module Reservations
+    class ReservationMailer < ActionMailer::Base
+
+      def confirmation(reservation, request)
+        @reservation = reservation
+        mail :subject   => Refinery::Reservations::Setting.confirmation_subject(Globalize.locale),
+             :to        => reservation.email,
+             :from      => "\"#{Refinery::Core.site_name}\" <no-reply@#{request.domain}>",
+             :reply_to  => Refinery::Reservations::Setting.notification_recipients.split(',').first
+      end
+
+      def notification(reservation, request)
+        @reservation = reservation
+        mail :subject   => Refinery::Reservations::Setting.notification_subject,
+             :to        => Refinery::Reservations::Setting.notification_recipients,
+             :from      => "\"#{Refinery::Core.site_name}\" <no-reply@#{request.domain}>",
+             :reply_to  => reservation.email
+      end
+
+    end
+  end
+end
