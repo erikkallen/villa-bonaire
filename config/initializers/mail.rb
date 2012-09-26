1ef6727231c6c14c53e48d791c3f32f6c70e19e3
Erik Kallen <info@erikkallen.nl>
2012-09-26 21:44:34 +0200

    fixed mail

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.delivery_method = :smtp 
-ActionMailer::Base.smtp_settings = { 
-  :address => 'localhost', 
-  :domain => 'www.villa-bonaire.nl.com', 
-  :port => 25 
-} 
+#ActionMailer::Base.smtp_settings = { 
+#  :address => 'localhost', 
+#  :domain => 'www.villa-bonaire.nl.com', 
+#  :port => 25 
+#} 
