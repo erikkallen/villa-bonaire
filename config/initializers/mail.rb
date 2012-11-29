ab4d86d60a4b3453f977faf87e8e1b6ceb5f8285
Erik Kallen <info@erikkallen.nl>
2012-11-29 21:50:41 +0100

    added email settings

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,6 +1,7 @@
-ActionMailer::Base.delivery_method = :smtp 
-#ActionMailer::Base.smtp_settings = { 
-#  :address => 'localhost', 
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address => 'localhost',
+  :enable_starttls_auto => false,
 #  :domain => 'www.villa-bonaire.nl.com', 
-#  :port => 25 
-#} 
+  :port => 25
+}
